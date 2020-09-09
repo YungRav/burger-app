ffdebc0523d51c8a9fa4d0699b86036113b85954
Carter Keith <carterkeith3gmail.com>
2020-09-08 20:59:26 -0400

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('The Rustico');
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Impossible Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (name) VALUES ('Big Mac');
